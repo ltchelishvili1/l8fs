48b75147959b58359064d4ce5c26c77ed5c8c836
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-03-23 10:26:09 +0400

    feat: add routing on landing page

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,15 @@ Route::get('/', function () {
     return view('posts');
 });
 
+
+Route::get('/posts/{post}', function ($slug) {
+    if(! file_exists($path = __DIR__ ."/../resources/posts/{$slug}.html")){
+        return redirect('/');
+    }
+
+    $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+
+    
+    return view('post',['post' => $post]);
+
+})-> where('post', '[A-z_\-]+');
