0e40fd175b02e5918c57d9b03d1ead306f1f878a
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-03-23 09:51:45 +0400

    feat: add landing page

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel='stylesheet' href='/app.css'>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque consequuntur sint enim, mollitia odit aliquid
+            eveniet, atque est eum sit fugit totam iure laudantium magni, id exercitationem obcaecati ipsa nesciunt.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque consequuntur sint enim, mollitia odit aliquid
+            eveniet, atque est eum sit fugit totam iure laudantium magni, id exercitationem obcaecati ipsa nesciunt.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque consequuntur sint enim, mollitia odit aliquid
+            eveniet, atque est eum sit fugit totam iure laudantium magni, id exercitationem obcaecati ipsa nesciunt.
+        </p>
+    </article>
+
+</body>
