6fcd095d7936e46d95f4b92c834bdc23f6308517
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-03-24 11:52:41 +0400

    refactor: combine factories into Post Factory

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Models\Post;
 Route::get('/', function () {
 
     return view('posts',[
-        'posts' => Post::with('category')-> get()
+        'posts' => Post::latest()->with('category')-> get()
     ]);
 });
 
