54fc994a33cd6999201c1b4459b0eecc994490c4
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-03-25 13:17:21 +0400

    feat: dynamic categories filter

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,8 @@ use App\Models\User;
 Route::get('/', function () {
 
     return view('posts',[
-        'posts' => Post::latest()-> get()
+        'posts' => Post::latest()-> get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -25,7 +26,9 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function(Category $category){
 
     return view('posts',[
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 
 });
@@ -35,6 +38,7 @@ Route::get('categories/{category:slug}', function(Category $category){
 Route::get('/authors/{author:username}', function (User $author) {
     
     return view('posts',[
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
