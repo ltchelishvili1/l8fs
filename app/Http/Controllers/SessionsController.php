1a31f4bc5cbbb2478415529144a68c7783d3644d
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-03-27 02:24:02 +0400

    feat: add login route

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,11 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+
+    public function create()
+    {
+      return view('sessions.create');
+    }
+
+    public function store()
+    {
+      $attributes = request()->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+      ]);
+
+      if(! auth()->attempt($attributes)){
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credential could not be verified.'
+        ]);
+      }
+      
+      session()->regenerate();
+
+      return redirect('/')->with('success', 'Welcome Back!');
+
+}
+
     public function destroy()
     {
         auth()->logout();
+
         return redirect('/')->with('success', 'Goodbye');
     }
 }
