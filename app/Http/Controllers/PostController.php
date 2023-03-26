d62459ae1111e460c38b3261f5695bb387f3fe7b
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-03-26 18:57:02 +0400

    feat: Add Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -11,7 +10,9 @@ class PostController extends Controller
     {
        
         return view('posts.index',[
-            'posts' => Post::latest()->filter(request(['search','category','author']))->get()
+            'posts' => Post::latest()->filter(
+                        request(['search','category','author'])
+                    )->paginate(6)->withQueryString()
         ]);
     }
 
