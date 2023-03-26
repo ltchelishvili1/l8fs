89a94b5b44e69e55244064cd1c202e39cebd0aba
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-03-26 12:10:00 +0400

    refactor: replace authors route with authors query

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,9 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Category;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-use App\Models\User;
+
 
 Route::get('/', [PostController::class, 'index'])->name('home');
-
-
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
-
-Route::get('/authors/{author:username}', function (User $author) {
-    
-    return view('posts',[
-        'posts' => $author->posts
-    ]);
-});
