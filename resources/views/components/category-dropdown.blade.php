714bee86978b69dcc144bd6b786770297dd5aed4
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-03-26 17:29:06 +0400

    feat: search inside categories

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -22,7 +22,7 @@
 >All</a>
 
 @foreach ($categories as $category)
-    <a href = '/?category={{$category->slug}}' 
+    <a href = '/?category={{$category->slug}}&{{http_build_query(request()->except('category'))}}' 
     class='block text-left px-3 text-sm leading-6 
     hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white
     {{isset($currentCategory) && $currentCategory->is($category)  ? 'bg-blue-500 text-white' : ''}}' 
