7d21c00c17300c2b9a965e23c66b104c8903cdb7
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-03-29 10:03:13 +0400

    refactor: extract components

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -4,103 +4,11 @@
             <h1 class="text-center font-bold text-xl">Register!</h1>
             <form method="POST" action="/register" class="mt-10 ">
                 @csrf
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                            for = 'name'
-                    >
-                        Name            
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                            type="text"
-                            name="name"
-                            id="name"
-                            value="{{old('name')}}"
-                            required
-                    >
-
-                    @error('name')
-                        <p class="text-red-500 text-sm mt-1">{{$message}}</p>
-                    @enderror
-
-                 
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                            for = 'username'
-                    >
-                        Username            
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                            type="text"
-                            name="username"
-                            id="username"
-                            value="{{old('username')}}"
-                            required
-                    >
-                 
-                    @error('username')
-                    <p class="text-red-500 text-sm mt-1">{{$message}}</p>
-                    @enderror
-
-                </div>
-
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                            for = 'email'
-                    >
-                        Email            
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                            type="email"
-                            name="email"
-                            id="email"
-                            value="{{old('email')}}"
-                            required
-                    >
-
-                    @error('email')
-                    <p class="text-red-500 text-sm mt-1">{{$message}}</p>
-                @enderror
-                 
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                            for = 'Password'
-                    >
-                        Password            
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                            type="password"
-                            name="password"
-                            id="password"
-                            required
-                    >
-
-                    @error('password')
-                    <p class="text-red-500 text-sm mt-1">{{$message}}</p>
-                    @enderror
-                 
-                </div>
-
-                <div class="mb-6">
-                    <button 
-                        class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
-                    >
-                        Submit
-                    </button>  
-                </div>
-{{-- 
-                @if ($errors->any())
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li class='text-red-500 text-xs'>{{$error}}</li>
-                        @endforeach
-                    </ul>
-                @endif --}}
+                <x-form.input name="name" />
+                <x-form.input name="username" />
+                <x-form.input name="email" type='email'/>
+                <x-form.input name="password" type="password" autocomplete="new-password"/>
+                <x-form.button>Submit</x-form.button>
             </form>
         </main>
     </section>
